9d8bcd31845d3c89891fbccf69e3f80574599eba
Jake Howard <git@theorangeone.net>
2021-09-05 16:00:59 +0100

    Version 0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setup(
     name="django-plausible",
-    version="0.0.1",
+    version="0.1.0",
     url="https://github.com/RealOrangeOne/django-plausible",
     author="Jake Howard",
     description=" Django module to provide easy Plausible integration, with Wagtail support",
